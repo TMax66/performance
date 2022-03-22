960e40fd418991b0e45d32fb97315b48d754381e
TRANQUILLO VITO <vito.tranquillo@izsler.it>
2022-03-22 10:50:56 +0100

    update

--- valutazioneDxsindacati.R
+++ valutazioneDxsindacati.R
@@ -8,10 +8,11 @@ library(gt)
 
 valdip <- read_excel("valutazioniD.xls", 
                      col_types = c("text", "text", "text", 
-                                   "numeric", "text", "numeric"))
+                                   "numeric", "text", "numeric", "text"))
 
 
 valdip %>% 
+  filter(escludere=="no") %>% 
   mutate(ruolo= ifelse(`DESCRIZIONE QUALIFICA` == "Dirigente veterinario", "VET", 
                        ifelse(`DESCRIZIONE QUALIFICA` == "Dirigente amministrativo", "AMM", 
                               ifelse(`DESCRIZIONE QUALIFICA` == "Dirigente veterinario a rapp.esclusivo", "VET", "SAN(C/B)")))) %>% 
@@ -21,20 +22,21 @@ valdip %>%
   count() %>%   
   filter(anno == 2021) %>% ungroup() %>% 
   select(- anno) %>% 
-  pivot_wider(names_from = "ruolo", values_from = "n", values_fill = 0 ) %>%   
-  gt() %>% 
-  gtsave("2021ruolo.rtf")
+  pivot_wider(names_from = "ruolo", values_from = "n", values_fill = 0 ) %>%   View()
+  # gt() %>% 
+  # gtsave("2020ruolo.rtf")
 
 
 valdip %>% 
+  filter(escludere=="no") %>% 
   mutate(ruolo= ifelse(`DESCRIZIONE QUALIFICA` == "Dirigente veterinario", "VET", 
                        ifelse(`DESCRIZIONE QUALIFICA` == "Dirigente amministrativo", "AMM", 
                               ifelse(`DESCRIZIONE QUALIFICA` == "Dirigente veterinario a rapp.esclusivo", "VET", "SAN(C/B)")))) %>% 
   filter(ruolo != "AMM") %>%  
-  group_by(anno, ruolo, Incarico) %>% 
+  group_by(anno, ruolo, Incarico) %>%
   summarise(n = n(), 
             totale = sum(TOT, na.rm = TRUE)) %>%  
-  filter(anno == 2021) %>% View()
+  filter(anno == 2020) %>% View()
   select(- anno) %>% 
   pivot_wider(names_from = "ruolo", values_from = "n", values_fill = 0 ) %>%   View()
   gt() %>% 
