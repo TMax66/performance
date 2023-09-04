2b22b1b2cb94b778a028d583aa4ac56df69ec9d7
massimo tranquillo <massimo.tranquillo@@gmail.com>
2023-09-04 12:42:09 +0200

    update

--- Codici Performance.R
+++ Codici Performance.R
@@ -42,6 +42,9 @@ query <- myfun(con=conSB, q=q, tabella = "vSchedaBudget")
 dati <- conSB %>% tbl(sql(query)) %>% as_tibble()
 
 
+
+dati <- readRDS("datiperformance.RDS")
+
 # perf <- dati %>% 
 #   filter(!str_detect(ObiettivoOperativo,"2.1.9."), 
 #          Anno == "2022", 
@@ -52,17 +55,45 @@ dati <- conSB %>% tbl(sql(query)) %>% as_tibble()
 
 
 
-perf <-  perf %>% 
-  filter(Periodo == 1) %>% 
-  mutate( AS = substr(AreaStrategica, start = 1, stop = 3), 
+ 
+
+perf <-  dati %>% 
+  filter(Anno == "2023", 
+    Periodo == 1) %>% 
+  mutate(Dipartimento = ifelse(is.na(Dipartimento), "DIREZIONE AMMINISTRATIVA", Dipartimento), 
+    AreaStrategica = recode(AreaStrategica, 
+                                 "AS1 ATTIVITA' ISITUZIONALE - GARANTIRE L'ATTIVITÀ ISTITUZIONALE IN MODO EFFICACE ED APPROPRIATO " = 
+                                   "AS1 ATTIVITA' ISITUZIONALE - GARANTIRE L'ATTIVITÀ ISTITUZIONALE IN MODO EFFICACE ED APPROPRIATO", 
+                                 "AS1 GARANTIRE L'ATTIVITA' ISTITUZIONALE IN MODO EFFICACE ED APPROPRIATO " =
+                                   "AS1 ATTIVITA' ISITUZIONALE - GARANTIRE L'ATTIVITÀ ISTITUZIONALE IN MODO EFFICACE ED APPROPRIATO", 
+                                 "AS1 GARANTIRE L'ATTIVITA' ISTITUZIONALE IN MODO EFFICACE ED APPROPRIATO" = 
+                                   "AS1 ATTIVITA' ISITUZIONALE - GARANTIRE L'ATTIVITÀ ISTITUZIONALE IN MODO EFFICACE ED APPROPRIATO",
+                                 
+                                 "AS2 POTENZIARE LE ATTIVITA' RELATIVE ALLA RICERCA NAZIONALE ED INTERNAZIONALE  " =
+                                   "AS2 POTENZIARE LE ATTIVITA' RELATIVE ALLA RICERCA NAZIONALE ED INTERNAZIONALE", 
+                                 
+                                 "AS3 SISTEMA GESTIONALE - GARANTIRE L’EFFICIENZA DEI SISTEMI GESTIONALI ATTRAVERSO LA SOSTENIBILITÀ DEI PROCESSI IN UN’OTTICA MULTIDIMENSIONALE "=
+                                   "AS3 SISTEMA GESTIONALE - GARANTIRE L’EFFICIENZA DEI SISTEMI GESTIONALI ATTRAVERSO LA SOSTENIBILITÀ DEI PROCESSI IN UN’OTTICA MULTIDIMENSIONALE", 
+                                 "AS3 GARANTIRE L’EFFICIENZA DEI SISTEMI GESTIONALI ATTRAVERSO LA SOSTENIBILITÀ DEI PROCESSI IN UN’OTTICA MULTIDIMENSIONALE  " =
+                                   "AS3 SISTEMA GESTIONALE - GARANTIRE L’EFFICIENZA DEI SISTEMI GESTIONALI ATTRAVERSO LA SOSTENIBILITÀ DEI PROCESSI IN UN’OTTICA MULTIDIMENSIONALE",
+                                 "AS3 GARANTIRE L’EFFICIENZA DEI SISTEMI GESTIONALI ATTRAVERSO LA SOSTENIBILITÀ DEI PROCESSI IN UN’OTTICA MULTIDIMENSIONALE " =
+                                   "AS3 SISTEMA GESTIONALE - GARANTIRE L’EFFICIENZA DEI SISTEMI GESTIONALI ATTRAVERSO LA SOSTENIBILITÀ DEI PROCESSI IN UN’OTTICA MULTIDIMENSIONALE",
+                                 
+                                 "AS4 FORMAZIONE E COMUNICAZIONE - COMUNICAZIONE\r\nPROMUOVERE LA FORMAZIONE CONTINUA, SVILUPPARE LA COMUNICAZIONE E VALORIZZAZIONE DELLE RISORSE UMANE\r\n" =
+                                   "AS4 FORMAZIONE E COMUNICAZIONE - COMUNICAZIONE PROMUOVERE LA FORMAZIONE CONTINUA, SVILUPPARE LA COMUNICAZIONE E VALORIZZAZIONE DELLE RISORSE UMANE", 
+                                 "AS4 PROMUOVERE LA FORMAZIONE CONTINUA, IL DIALOGO CON GLI STAKEHOLDER E  LA VALORIZZAZIONE DELLE RISORSE UMANE " =
+                                   "AS4 FORMAZIONE E COMUNICAZIONE - COMUNICAZIONE PROMUOVERE LA FORMAZIONE CONTINUA, SVILUPPARE LA COMUNICAZIONE E VALORIZZAZIONE DELLE RISORSE UMANE"
+                                 ), 
+    AS = substr(AreaStrategica, start = 1, stop = 3), 
           AreaStrategica = recode( AS, 
                                    AS1 = "AS1-ATTIVITA' ISTITUZIONALE", 
                                    AS2 = "AS2-POTENZIAMENTO DELLA RICERCA", 
                                    AS3 = "AS3-SISTEMA GESTIONALE", 
-                                   AS4 = "AS4-FORMAZIONE E COMUNICAZIONE", 
-                                   AS5 = "AS5-ETICA E LEGALITA’"),
+                                   AS4 = "AS4-FORMAZIONE E COMUNICAZIONE"), 
+                                   
           Dipartimento = factor(Dipartimento, levels = c("DIREZIONE GENERALE", 
                                                          "DIREZIONE SANITARIA", 
+                                                         "DIREZIONE AMMINISTRATIVA", 
                                                          "DIPARTIMENTO AMMINISTRATIVO",
                                                          "DIPARTIMENTO TUTELA SALUTE ANIMALE", 
                                                          "DIPARTIMENTO SICUREZZA ALIMENTARE", 
@@ -79,16 +110,16 @@ perf %>%
 
  
 
-perf %>% rename( "Area Strategica" = AreaStrategica ) %>%   
-  #write.xlsx(file= "LSE.xlsx")
+# perf %>% rename( "Area Strategica" = AreaStrategica ) %>%   
+#   #write.xlsx(file= "LSE.xlsx")
 
 
 library(flexdashboard)
-gauge(97.2, min= 0, max = 100, symbol = '%',
+gauge(46, min= 0, max = 100, symbol = '%',
       gaugeSectors(success = c(0,100),   colors = "steelblue"))
 
-
-  group_by(`Area Strategica`) %>% 
+perf %>% 
+  group_by(AreaStrategica) %>% 
   summarise(media = 100*mean(Avanzamento,na.rm  = T)) %>%
   ungroup() %>%  
   #pivot_wider(names_from = Dipartimento, values_from = media) %>%  
@@ -102,23 +133,209 @@ gauge(97.2, min= 0, max = 100, symbol = '%',
 
 
 perf %>% 
-  mutate(Dipartimento = factor(Dipartimento, levels =  c("DIREZIONE GENERALE", 
-                                                         "DIREZIONE SANITARIA", 
-                                                         "DIPARTIMENTO AMMINISTRATIVO",
-                                                         "DIPARTIMENTO TUTELA SALUTE ANIMALE", 
-                                                         "DIPARTIMENTO SICUREZZA ALIMENTARE", 
-                                                         "DIPARTIMENTO AREA TERRITORIALE LOMBARDIA",
-                                                         "DIPARTIMENTO AREA TERRITORIALE EMILIA ROMAGNA"))) %>%   
-  group_by(AreaStrategica, Dipartimento) %>% 
+  group_by(AreaStrategica, Dipartimento)  %>% 
   summarise(media = 100*mean(Avanzamento,na.rm  = T)) %>%
   ungroup() %>% 
   pivot_wider(names_from = AreaStrategica, values_from = media) %>%   
   gt() %>% 
-  fmt_number(columns = 2:6,decimals = 2) %>% 
+  fmt_number(columns = 2:5,decimals = 2) %>% 
   sub_missing(
-    columns = 2:6,
+    columns = 2:5,
     missing_text = "") 
  
+  
+  
+  
+  
+  
+
+  
+
+Area <-  perf %>%
+  filter(Periodo == 1  ) %>%
+  mutate(MacroArea = factor(MacroArea)) %>%
+  group_by(MacroArea) %>%
+  summarise(mediana =  round(median(Avanzamento, na.rm = T),2),
+            media = round(mean(Avanzamento,na.rm  = T),2),
+            n = n()) %>%
+  mutate(mediana = percent(mediana),
+         mediana = as.character(mediana),
+         media = percent(media),
+         media = as.character(media)) %>%
+  #pivot_wider(names_from = "Dipartimento", values_from = "mediana", values_fill = " ") %>%
+  arrange(MacroArea) %>%
+  mutate(MacroArea = as.character(MacroArea)) %>%
+  mutate(MacroArea = gsub("\\d+", "", MacroArea),
+         MacroArea = gsub("\"", "", MacroArea))  %>%
+  kbl( ) %>%
+  kable_styling() %>%
+  kable_paper(bootstrap_options = "striped", full_width = F)
+  # 
+  # 
+  # #Polar plot avanzamento per Area----
+  # 
+  plot_dt <- perf %>%
+      filter(Periodo == 1  ) %>%
+    # mutate(MacroArea = factor(MacroArea)) %>%
+    group_by(AreaStrategica) %>%
+    summarise(mediana =  100*round(median(Avanzamento, na.rm = T),2),
+              media = 100*round(mean(Avanzamento, na.rm = T),2),
+              n = n()) %>%
+    mutate(target = 100)  
+    # mutate(MacroArea = as.character(MacroArea)) %>%
+    # mutate(MacroArea = gsub("\\d+", "", MacroArea),
+    #        MacroArea = gsub("\"", "", MacroArea))
+
+  # #Polar plot avanzamento per Area/Dip---
+  # plot_dt2 <- dt %>%
+  #   mutate(MacroArea = factor(MacroArea)) %>%
+  #   group_by(MacroArea, Dipartimento) %>%
+  #   summarise(mediana =  100*round(median(Avanzamento, na.rm = T),2),
+  #             media = 100*round(mean(Avanzamento, na.rm = T),2),
+  #             n = n()) %>%
+  #   mutate(target = 100) %>%
+  #   mutate(MacroArea = as.character(MacroArea)) %>%
+  #   mutate(MacroArea = gsub("\\d+", "", MacroArea),
+  #          MacroArea = gsub("\"", "", MacroArea))
+  # 
+  # 
+  # 
+  # 
+  # 
+  # ##Plot----
+  plt <- ggplot(plot_dt)+
+    geom_hline(
+      aes(yintercept = y),
+      data.frame(y = c(0, 25, 50, 75, 90, 100)),
+      color = "lightgrey"
+    )+
+    geom_col(
+      aes(x = reorder(str_wrap(AreaStrategica, 1), media),
+          y = media,
+          fill = media
+          ),
+      position = "dodge2",
+      show.legend = TRUE,
+      alpha = .9
+    )+
+
+    geom_point(
+      aes(
+        x = reorder(str_wrap(AreaStrategica, 1), media),
+        y = media
+      ),
+      size = 3, color = "gray12"
+    )+
+
+    geom_segment(
+      aes(
+        x =  reorder(str_wrap(AreaStrategica, 1), media),
+        y = 0,
+        xend = reorder(str_wrap(AreaStrategica, 1), media),
+        yend = 100
+      ),
+      linetype = "dashed",
+      color = "gray12"
+    )+
+    coord_polar()+
+
+    scale_y_continuous(
+      limits = c(-20,110),
+      expand = c(0, 0)
+
+    ) +
+    geom_text(
+      aes(
+        x = reorder(str_wrap(AreaStrategica, 1), media),
+        y = media-10,
+        label = paste0(media, "%")),
+        color = "black",
+        size=5)+
+
+    annotate(
+      x = 0.5,
+      y = 30,
+      label = "25%",
+      geom = "text",
+      color = "red",
+      family = "Bell MT"
+    )  +
+    annotate(
+      x = 0.5,
+      y = 55,
+      label = "50%",
+      geom = "text",
+      color = "red",
+      family = "Bell MT"
+    )  +
+
+    annotate(
+      x = 0.5,
+      y = 80,
+      label = "75%",
+      geom = "text",
+      color = "red",
+      family = "Bell MT"
+    )  +
+
+    annotate(
+      x = 0.5,
+      y = 110,
+      label = "100%",
+      geom = "text",
+      color = "red",
+      family = "Bell MT"
+    )  +
+
+   # scale_fill_gradientn(colours = gray.colors(7))+
+
+    theme(
+      # Remove axis ticks and text
+      axis.title = element_blank(),
+      axis.ticks = element_blank(),
+      axis.text.y = element_blank(),
+      # Use gray text for the region names
+      axis.text.x = element_text(color = "gray12", size = 8),
+      # Move the legend to the bottom
+      legend.position = "blank",
+    )+
+
+    # Customize general theme
+    theme(
+
+      # Set default color and font family for the text
+      text = element_text(color = "gray12", family = "Bell MT"),
+
+      # Customize the text in the title, subtitle, and caption
+      plot.title = element_text(face = "bold", size = 18),
+      plot.subtitle = element_text(size = 14, hjust = 0.05),
+      plot.caption = element_text(size = 10, hjust = .5),
+
+      # Make the background white and remove extra grid lines
+      panel.background = element_rect(fill = "white", color = "white"),
+      panel.grid = element_blank(),
+      panel.grid.major.x = element_blank()
+    )
+  # 
+  # 
+  # 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
 
 library(strex)
 
@@ -383,183 +600,6 @@ perf %>%
 # 
 # 
 # #Dipartimenti----
-# 
-# dt %>% 
-#   filter(Periodo == 2 & Avanzamento != 0 ) %>% View()
-#   #group_by(MacroArea) %>% 
-#   summarise(media = 100*round(mean(Avanzamento,na.rm  = T),2)) %>% View()
-#   
-# # filter(Dipartimento == "Direzione Generale")
-#   
-#   
-# 
-# #Avanzamento per Area----
-# Area <-  perf %>%
-#     filter(Periodo == 2 & Avanzamento != 0 ) %>% 
-#   mutate(MacroArea = factor(MacroArea)) %>%  
-#   group_by(MacroArea) %>% 
-#   summarise(mediana =  round(median(Avanzamento, na.rm = T),2),
-#             media = round(mean(Avanzamento,na.rm  = T),2), 
-#             n = n()) %>% 
-#   mutate(mediana = percent(mediana), 
-#          mediana = as.character(mediana), 
-#          media = percent(media),
-#          media = as.character(media)) %>%   
-#   #pivot_wider(names_from = "Dipartimento", values_from = "mediana", values_fill = " ") %>%  
-#   arrange(MacroArea) %>% 
-#   mutate(MacroArea = as.character(MacroArea)) %>% 
-#   mutate(MacroArea = gsub("\\d+", "", MacroArea), 
-#          MacroArea = gsub("\"", "", MacroArea))  %>%  
-#   kbl( ) %>% 
-#   kable_styling() %>% 
-#   kable_paper(bootstrap_options = "striped", full_width = F)
-# 
-# 
-# #Polar plot avanzamento per Area----
-# 
-# plot_dt <- dt %>%  
-#     filter(Periodo == 2 & Avanzamento != 0 ) %>% 
-#   mutate(MacroArea = factor(MacroArea)) %>% 
-#   group_by(MacroArea) %>% 
-#   summarise(mediana =  100*round(median(Avanzamento, na.rm = T),2),
-#             media = 100*round(mean(Avanzamento, na.rm = T),2), 
-#             n = n()) %>% 
-#   mutate(target = 100) %>% 
-#   mutate(MacroArea = as.character(MacroArea)) %>% 
-#   mutate(MacroArea = gsub("\\d+", "", MacroArea), 
-#          MacroArea = gsub("\"", "", MacroArea))
-#   
-# #Polar plot avanzamento per Area/Dip---
-# plot_dt2 <- dt %>%  
-#   mutate(MacroArea = factor(MacroArea)) %>% 
-#   group_by(MacroArea, Dipartimento) %>% 
-#   summarise(mediana =  100*round(median(Avanzamento, na.rm = T),2),
-#             media = 100*round(mean(Avanzamento, na.rm = T),2), 
-#             n = n()) %>% 
-#   mutate(target = 100) %>% 
-#   mutate(MacroArea = as.character(MacroArea)) %>% 
-#   mutate(MacroArea = gsub("\\d+", "", MacroArea), 
-#          MacroArea = gsub("\"", "", MacroArea))
-# 
-# 
-# 
-# 
-# 
-# ##Plot----
-# plt <- ggplot(plot_dt)+
-#   geom_hline(
-#     aes(yintercept = y),
-#     data.frame(y = c(0, 25, 50, 75, 90, 100)), 
-#     color = "lightgrey"
-#   )+
-#   geom_col(
-#     aes(x = reorder(str_wrap(MacroArea, 1), media), 
-#         y = media, 
-#         fill = media
-#         ), 
-#     position = "dodge2", 
-#     show.legend = TRUE, 
-#     alpha = .9
-#   )+
-#     
-#   geom_point(
-#     aes(
-#       x = reorder(str_wrap(MacroArea, 1), media),
-#       y = media
-#     ), 
-#     size = 3, color = "gray12"
-#   )+
-#     
-#   geom_segment(
-#     aes(
-#       x =  reorder(str_wrap(MacroArea, 1), media), 
-#       y = 0, 
-#       xend = reorder(str_wrap(MacroArea, 1), media), 
-#       yend = 100
-#     ), 
-#     linetype = "dashed",
-#     color = "gray12"
-#   )+
-#   coord_polar()+
-#   
-#   scale_y_continuous(
-#     limits = c(-20,110),
-#     expand = c(0, 0)
-#     
-#   ) +
-#   geom_text(
-#     aes(
-#       x = reorder(str_wrap(MacroArea, 1), media),
-#       y = media-10, 
-#       label = paste0(media, "%")), 
-#       color = "black", 
-#       size=5)+
-#   
-#   annotate(
-#     x = 0.5, 
-#     y = 30, 
-#     label = "25%", 
-#     geom = "text", 
-#     color = "red", 
-#     family = "Bell MT"
-#   )  +
-#   annotate(
-#     x = 0.5, 
-#     y = 55, 
-#     label = "50%", 
-#     geom = "text", 
-#     color = "red", 
-#     family = "Bell MT"
-#   )  +
-#   
-#   annotate(
-#     x = 0.5, 
-#     y = 80, 
-#     label = "75%", 
-#     geom = "text", 
-#     color = "red", 
-#     family = "Bell MT"
-#   )  +
-#   
-#   annotate(
-#     x = 0.5, 
-#     y = 110, 
-#     label = "100%", 
-#     geom = "text", 
-#     color = "red", 
-#     family = "Bell MT"
-#   )  +
-#   
-#  # scale_fill_gradientn(colours = gray.colors(7))+
-#   
-#   theme(
-#     # Remove axis ticks and text
-#     axis.title = element_blank(),
-#     axis.ticks = element_blank(),
-#     axis.text.y = element_blank(),
-#     # Use gray text for the region names
-#     axis.text.x = element_text(color = "gray12", size = 8),
-#     # Move the legend to the bottom
-#     legend.position = "blank",
-#   )+
-# 
-#   # Customize general theme
-#   theme(
-#     
-#     # Set default color and font family for the text
-#     text = element_text(color = "gray12", family = "Bell MT"),
-#     
-#     # Customize the text in the title, subtitle, and caption
-#     plot.title = element_text(face = "bold", size = 18),
-#     plot.subtitle = element_text(size = 14, hjust = 0.05),
-#     plot.caption = element_text(size = 10, hjust = .5),
-#     
-#     # Make the background white and remove extra grid lines
-#     panel.background = element_rect(fill = "white", color = "white"),
-#     panel.grid = element_blank(),
-#     panel.grid.major.x = element_blank()
-#   )
-# 
 # 
 # 
 # 
